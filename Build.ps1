59b19cd3129c8f56ec4401bfe7a400a64cfc294d
Henning Bjørgo <hbjorgo@users.noreply.github.com>
2021-06-30 22:03:54 +0200

    Build script

--- Build.ps1
+++ Build.ps1
@@ -19,13 +19,13 @@ dotnet pack .\src\HeboTech.Fruits\HeboTech.Fruits.csproj -c Release -o $artifact
 
 ### Fruits ASP.Net Core Library ###
 Write-Host "### dotnet clean... ###"
-dotnet clean .\src\HeboTech.Fruits\HeboTech.Fruits.csproj -c Release
+dotnet clean .\src\HeboTech.Fruits.AspNetCore\HeboTech.Fruits.AspNetCore.csproj -c Release
 
 Write-Host "### dotnet build... ###"
-dotnet build .\src\HeboTech.Fruits\HeboTech.Fruits.csproj -c Release
+dotnet build .\src\HeboTech.Fruits.AspNetCore\HeboTech.Fruits.AspNetCore.csproj -c Release
 
 Write-Host "### dotnet test... ###"
-dotnet test .\src\Tests\HeboTech.Fruits.Tests\HeboTech.Fruits.Tests.csproj -c Release
+dotnet test .\src\Tests\HeboTech.Fruits.AspNetCore.Tests\HeboTech.Fruits.AspNetCore.Tests.csproj -c Release
 
 Write-Host "### dotnet pack... ###"
-dotnet pack .\src\HeboTech.Fruits\HeboTech.Fruits.csproj -c Release -o $artifacts --no-build
+dotnet pack .\src\HeboTech.Fruits.AspNetCore\HeboTech.Fruits.AspNetCore.csproj -c Release -o $artifacts --no-build
